509538cd6c257496119db76cda8645c6ca4f5cd9
moonpeter <moonpeter@kakao.com>
2020-03-05 23:09:14 +0900

    update for 클래스 제네릭 뷰

--- app/crawling/views.py
+++ app/crawling/views.py
@@ -1,15 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView, RedirectView
 
 from .models import CrawlingData
 
 
-class DetailView(TemplateView):
-    template_name = 'crawling/crawling.html'
-
-# def detail(request):
-#     crawlings = CrawlingData.objects.all()
-#     context = {
-#         'crawlingdata': CrawlingData,
-#     }
-#     return render(request, 'crawling/crawling.html', context)
+class DetailView(View):
+    def get(self, request):
+        return render(request, 'crawling/crawling.html')
